5d4a7671551bb6baa715e99b4658faff2ac7768f
Ender Efe <efe.ender.er@gmail.com>
2025-10-30 00:22:18 +0300

    UI a little.

--- app.py
+++ app.py
@@ -3,12 +3,32 @@ from flask import Flask, request, jsonify, render_template, send_from_directory
 from dotenv import load_dotenv
 load_dotenv()
 
+import pdfplumber
+pdf_path = os.getenv("PDF_FOLDER")
+def extract_text_from_pdf(file_path):
+    texts = []
+    with pdfplumber.open(file_path) as pdf:
+        for page in pdf.pages:
+            texts.append(page.extract_text())
+    return texts
 
 app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def home():
-    return "Welcome to the Flask API!"
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_files():
+    uploaded_files = request.files.getlist("pdfs")
+    file_texts = []
+    for file in uploaded_files:
+        if file.filename.endswith('.pdf'):
+            file_path = os.path.join('uploads', file.filename)
+            file.save(file_path)
+            texts = extract_text_from_pdf(file_path)
+            file_texts.append({'filename': file.filename, 'text': texts})
+    return jsonify({'files': file_texts})
 
 
 
--- utils/pdf_util.py
+++ utils/pdf_util.py
@@ -1,5 +1,10 @@
 import pdfplumber
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+
+pdf_path = os.getenv("PDF_FOLDER")
 
 def extract_text_from_pdf(file_path):
     texts = []
@@ -7,3 +12,15 @@ def extract_text_from_pdf(file_path):
         for page in pdf.pages:
             texts.append(page.extract_text())
     return texts
+
+
+def main():
+    for filename in os.listdir(pdf_path):
+        if filename.endswith('.pdf'):
+            file_path = os.path.join(pdf_path, filename)
+            texts = extract_text_from_pdf(file_path)
+            print(f"Texts form for {filename}:")
+            print(texts)
+
+if __name__ == "__main__":
+    main()
